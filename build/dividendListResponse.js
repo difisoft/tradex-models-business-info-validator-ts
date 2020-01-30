a3ed79fbe184233f22811204d1e9f977be68ecda
schema-generator <developer@techx.vn>
2020-01-30 15:30:37 +0700

    schema genarator by buildNumber:

---
+++ build/dividendListResponse.js
@@ -0,0 +1,52 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Ajv = require("ajv");
+exports.dividendListResponseSchema = {
+    "id": "http://tradex.techx.vn/business-info/dividendListResponse",
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "description": "dividendListResponse schema",
+    "type": "object",
+    "properties": {
+        "id": {
+            "description": "id",
+            "type": "string"
+        },
+        "code": {
+            "description": "symbol code",
+            "type": "string"
+        },
+        "eventType": {
+            "description": "event type (DIVIDEND/PHK/RIGHT_ISSUE/BONUS_SHARE/PHCL...)",
+            "type": "string"
+        },
+        "exDividendDate": {
+            "description": "exDividend date",
+            "type": "string"
+        },
+        "implementDate": {
+            "description": "implement date",
+            "type": "string"
+        },
+        "ratio": {
+            "description": "ratio",
+            "type": "number"
+        },
+        "price": {
+            "description": "price",
+            "type": "number"
+        },
+        "comment": {
+            "description": "comment",
+            "type": "string"
+        }
+    }
+};
+let dividendListResponseAjv = null;
+function dividendListResponseValidator() {
+    if (dividendListResponseAjv == null) {
+        dividendListResponseAjv = new Ajv({ validateSchema: false }).compile(exports.dividendListResponseSchema);
+    }
+    return dividendListResponseAjv;
+}
+exports.dividendListResponseValidator = dividendListResponseValidator;
+//# sourceMappingURL=dividendListResponse.js.map
---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dividendListRequest_1 = require("./dividendListRequest");
+exports.dividendListRequestSchema = dividendListRequest_1.dividendListRequestSchema;
+exports.dividendListRequestValidator = dividendListRequest_1.dividendListRequestValidator;
+const dividendListResponse_1 = require("./dividendListResponse");
+exports.dividendListResponseSchema = dividendListResponse_1.dividendListResponseSchema;
+exports.dividendListResponseValidator = dividendListResponse_1.dividendListResponseValidator;
+const lastDividendRequest_1 = require("./lastDividendRequest");
+exports.lastDividendRequestSchema = lastDividendRequest_1.lastDividendRequestSchema;
+exports.lastDividendRequestValidator = lastDividendRequest_1.lastDividendRequestValidator;
+const lastDividendResponse_1 = require("./lastDividendResponse");
+exports.lastDividendResponseSchema = lastDividendResponse_1.lastDividendResponseSchema;
+exports.lastDividendResponseValidator = lastDividendResponse_1.lastDividendResponseValidator;
+//# sourceMappingURL=index.js.map
